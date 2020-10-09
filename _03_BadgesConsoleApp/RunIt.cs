14801ac6c79034d2bc101e0e8a6ae20c8bdac3ee
jhemphill1389 <ks.jah.1776@gmail.com>
2020-10-09 08:53:09 -0500

    challengenumber3

---
+++ _03_BadgesConsoleApp/RunIt.cs
@@ -0,0 +1,18 @@
+﻿using BadgesUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_BadgesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI ui = new UI();
+            ui.Run();
+        }
+    }
+}
